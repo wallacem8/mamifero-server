0e996c75aef93468b53d6d54ef78173ae9bf05c2
Wallace Medeiros <wallacemedeiros_97@hotmail.com>
2023-05-22 16:03:39 -0300

    initial migrations

---
+++ prisma/migrations/20230522190146_initial/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Javali" (
+    "nome" TEXT NOT NULL,
+    "peso" INTEGER NOT NULL,
+    "sexo" VARCHAR(255) NOT NULL,
+    "adulto" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Javali_pkey" PRIMARY KEY ("nome")
+);
